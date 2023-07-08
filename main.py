5947048b0042cbb92435c21ec541e88e81bccb95
Hammad740 <mohammadhammadansari91@gmail.com>
2023-07-08 22:14:55 +0530

    Add images,description and links

--- main.py
+++ main.py
@@ -21,13 +21,20 @@ user interfaces while seamlessly integrating them with efficient server-side log
 content2 = """Below  you can find some of the apps I have build in Python.Feel free to contact me !"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, rows in df[:10].iterrows():
         st.header(rows["title"])
-
+        st.write(rows["description"])
+        st.image("images/" + rows["image"])
+        # st.write("[Source Code](https://github.com/Hammad740/My-Todo-Web-App/tree/master)")
+        st.write(f"[Source Code]({rows['url']})")
 with col4:
-    for index,rows in df[10:].iterrows():
+    for index, rows in df[10:].iterrows():
         st.header(rows["title"])
+        st.write(rows["description"])
+        st.image("images/" + rows["image"])
+        # st.write("[Source Code](https://github.com/Hammad740/My-Todo-Web-App/tree/master)")
+        st.write(f"[Source Code]({rows['url']})")
